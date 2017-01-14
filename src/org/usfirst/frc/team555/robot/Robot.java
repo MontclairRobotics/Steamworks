8607a17d2abab1ababef0de6a8dc57f06b07aa31
Montclair Robotics <mhs@mpsdnj.us>
2017-01-14 10:18:08 -0500

    Initial robot code

--- src/org/usfirst/frc/team555/robot/Robot.java
+++ src/org/usfirst/frc/team555/robot/Robot.java
@@ -1,8 +1,14 @@
 package org.usfirst.frc.team555.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.montclairrobotics.sprocket.SprocketRobot;
+import org.montclairrobotics.sprocket.drive.DriveTrain;
+import org.montclairrobotics.sprocket.drive.DriveTrainBuilder;
+import org.montclairrobotics.sprocket.drive.InvalidDriveTrainException;
+import org.montclairrobotics.sprocket.geometry.Degrees;
+import org.montclairrobotics.sprocket.geometry.Position;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,70 +17,55 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends IterativeRobot {
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
-	SendableChooser<String> chooser = new SendableChooser<>();
-
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-	@Override
-	public void robotInit() {
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
-	}
-
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
+public class Robot extends SprocketRobot {
+	
+	Joystick stick;
+	DriveTrain driveTrain;
+	
 	@Override
 	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
+		// TODO Auto-generated method stub
+		super.autonomousInit();
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
 	@Override
 	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
-			break;
+		// TODO Auto-generated method stub
+		super.autonomousPeriodic();
+	}
+
+	@Override
+	public void robotInit() {
+		super.robotInit();
+		
+		stick = new Joystick(0);
+		
+		DriveTrainBuilder builder = new DriveTrainBuilder();
+		builder	.addWheel(new Talon(0), new Degrees(0), Position.FL)
+				.addWheel(new Talon(1), new Degrees(0), Position.FR)
+				.addWheel(new Talon(2), new Degrees(0), Position.BL)
+				.addWheel(new Talon(3), new Degrees(0), Position.BR);
+		builder.setArcadeDriveInput(stick);
+		try {
+			driveTrain = builder.build();
+		} catch (InvalidDriveTrainException e) {
+			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
 	@Override
-	public void teleopPeriodic() {
+	public void teleopInit() {
+		// TODO Auto-generated method stub
+		super.teleopInit();
 	}
 
-	/**
-	 * This function is called periodically during test mode
-	 */
 	@Override
-	public void testPeriodic() {
+	public void teleopPeriodic() {
+		// TODO Auto-generated method stub
+		super.teleopPeriodic();
 	}
+	
+	
+	
 }
 
