661749f3221c1cb9017341400d0fa123ff6416e5
Rafi Baum <rafi@ukbaums.com>
2017-02-04 12:59:54 -0500

    Created buttons for opening and closing

--- src/org/usfirst/frc/team555/robot/Robot.java
+++ src/org/usfirst/frc/team555/robot/Robot.java
@@ -19,53 +19,6 @@ import edu.wpi.first.wpilibj.Talon;
  */
 public class Robot extends SprocketRobot {
 	
-	Joystick stick;
-	DriveTrain driveTrain;
-	
-	@Override
-	public void autonomousInit() {
-		// TODO Auto-generated method stub
-		super.autonomousInit();
-	}
-
-	@Override
-	public void autonomousPeriodic() {
-		// TODO Auto-generated method stub
-		super.autonomousPeriodic();
-	}
-
-	@Override
-	public void robotInit() {
-		super.robotInit();
-		
-		stick = new Joystick(0);
-		
-		DriveTrainBuilder builder = new DriveTrainBuilder();
-		builder	.addWheel(new Talon(0), new Degrees(0), Position.FL)
-				.addWheel(new Talon(1), new Degrees(0), Position.FR)
-				.addWheel(new Talon(2), new Degrees(0), Position.BL)
-				.addWheel(new Talon(3), new Degrees(0), Position.BR);
-		builder.setArcadeDriveInput(stick);
-		try {
-			driveTrain = builder.build();
-		} catch (InvalidDriveTrainException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void teleopInit() {
-		// TODO Auto-generated method stub
-		super.teleopInit();
-	}
-
-	@Override
-	public void teleopPeriodic() {
-		// TODO Auto-generated method stub
-		super.teleopPeriodic();
-	}
-	
-	
 	
 }
 
