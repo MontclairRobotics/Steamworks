95310aee8eaa0382190aefa46472b0c40a077365
MHSRobotics <mhs@mpsdnj.us>
2017-02-21 20:39:23 -0500

    PID changes

--- src/org/usfirst/frc/team555/robot/Robot.java
+++ src/org/usfirst/frc/team555/robot/Robot.java
@@ -133,7 +133,7 @@ public class Robot extends SprocketRobot {
 		
 		//Gyro lock
 		NavXRollInput navX = new NavXRollInput(Port.kMXP);
-		PID gyroPID = new PID(1,0,.000);
+		PID gyroPID = new PID(1,0,.004);
 		gyroPID.setInput(navX);
 		GyroLock gLock = new GyroLock(gyroPID);
 		//Gyro lock button
