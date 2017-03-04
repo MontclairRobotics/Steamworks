400812797930977ed273c7f44f0ed83e7b9e7792
Rafi Baum <rafi@ukbaums.com>
2017-03-04 09:41:16 -0500

    Contours sorted by area

--- src/org/usfirst/frc/team555/robot/Vision.java
+++ src/org/usfirst/frc/team555/robot/Vision.java
@@ -1,8 +1,13 @@
 package org.usfirst.frc.team555.robot;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 import org.montclairrobotics.sprocket.loop.Priority;
 import org.montclairrobotics.sprocket.loop.Updatable;
 import org.montclairrobotics.sprocket.loop.Updater;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 
@@ -35,9 +40,20 @@ public class Vision implements Updatable{
 	    
 	    visionThread = new VisionThread(camera, new GripPipelineC(), pipeline -> {
 	    	int x,y;
-	        if (pipeline.filterContoursOutput().size()>0) {
-	            Rect a = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
-	            //Rect b = Imgproc.boundingRect(pipeline.filterContoursOutput().get(1));
+	    	ArrayList<MatOfPoint> contours = pipeline.filterContoursOutput();
+	        if (contours.size()>0) {
+	        	Collections.sort(contours, new Comparator<MatOfPoint>() {
+					@Override
+					public int compare(MatOfPoint o1, MatOfPoint o2) {
+						if(Imgproc.boundingRect(o1).area() >= Imgproc.boundingRect(o2).area()) {
+							return 1;
+						} else {
+							return -1;
+						}
+					}
+		    	});
+	            Rect a = Imgproc.boundingRect(contours.get(0));
+	            //Rect b = Imgproc.boundingRect(contours.get(1));
 	            Rect b=a;
                 x = (a.x + b.x)/2 + (a.width + b.width) / 4;
                 y = (a.y + b.y)/2 + (a.height + b.height) / 4;
