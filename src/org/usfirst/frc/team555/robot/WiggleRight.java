336f268f3e3b243d8669ac524dc3d4f0fd9713a4
MHSRobotics <mhs@mpsdnj.us>
2017-03-27 14:56:10 -0400

    Added auto wiggle classes

---
+++ src/org/usfirst/frc/team555/robot/WiggleRight.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team555.robot;
+
+import org.montclairrobotics.sprocket.auto.AutoState;
+import org.montclairrobotics.sprocket.geometry.Radians;
+import org.montclairrobotics.sprocket.geometry.XY;
+
+public class WiggleRight extends AutoState {
+
+	@Override
+	public boolean isDone() {
+		return timeInState() > .25;
+	}
+
+	@Override
+	public void stateUpdate() {
+		tgtDir = new XY(0, .1);
+		tgtTurn = new Radians(.1);
+	}
+
+}
